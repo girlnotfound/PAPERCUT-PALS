3cc0e420e05a7e66a9ad3d588581ca356916c2fc
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-09-06 19:04:53 -0700

    Added carousel to Homepage.jsx, added associated css to styles.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
+import Homepage from "./pages/Homepage";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="book-club-container">
       <Header />
       <Navigation />
       <main className="book-club-content">
-        <Outlet />
+        {location.pathname === "/" ? <Homepage /> : <Outlet />}
       </main>
       <Footer />
     </div>
