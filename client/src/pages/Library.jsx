9076c961d464134189073b62237ca1a9653fabb6
Renz <renzlakers24@gmail.com>
2024-09-12 20:11:28 -0700

    Added initial back-end implementation of favoriteBook

--- client/src/pages/Library.jsx
+++ client/src/pages/Library.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Box, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
 import BookCard from "../components/BookCard";
+import { ADD_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const Library = () => {
   const [books, setBooks] = useState([]);
   const [favorites, setFavorites] = useState([]);
-
+  const [addBook] = useMutation(ADD_BOOK);
   const bgGradient = useColorModeValue(
     "linear-gradient(-20deg, #d558c8 0%, #24d292 100%)",
     "linear-gradient(-20deg, #d558c8 0%, #24d292 100%)"
@@ -31,10 +33,33 @@ const Library = () => {
     }
   };
 
-  const addToFavorites = (book) => {
+  const addToFavorites = async (book) => {
     const updatedFavorites = [...favorites, book];
     setFavorites(updatedFavorites);
-    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    console.log(updatedFavorites.map(book => book.volumeInfo));
+  
+    try {
+      await Promise.all(updatedFavorites.map(async (book) => {
+        console.log(book.volumeInfo.title);
+        console.log(book.volumeInfo.authors.join(", "));
+        console.log(book.volumeInfo.categories.join(", "));
+        console.log(book.volumeInfo.description);
+        console.log(book.volumeInfo.publisher);
+        const { data } = await addBook({
+          variables: {
+            title: book.volumeInfo.title,
+            author: book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "Unknown",
+            genre: book.volumeInfo.categories ? book.volumeInfo.categories.join(", ") : "Uncategorized",
+            synopsis: book.volumeInfo.description || "No description available",
+            publisher: book.volumeInfo.publisher || "Unknown"
+          }
+        });
+        console.log("Book added:", data.addBook);
+      }));
+      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    } catch (error) {
+      console.error("Error adding books to favorites:", error);
+    }
   };
 
   const removeFromFavorites = (book) => {
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,3 +24,34 @@ export const LOGIN_USER = gql`
   }
 `;
 
+
+export const ADD_COMMENT = gql`
+  mutation addComment($favoriteBookId: ID!, $commentText: String!) {
+    addComment(favoriteBookId: $favoriteBookId, commentText: $commentText) {
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
+
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $genre: String!, $synopsis: String!, $publisher: String!) {
+    addBook(title: $title, author: $author, genre: $genre, synopsis: $synopsis, publisher: $publisher) {
+      _id
+      title
+      author
+      genre
+      synopsis
+      publisher
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
